6d61f1b850f1f9412ec8278f8369d94a940334b8
Marcos casa <mefsvlr@gmail.com>
2026-09-18 19:30:03 -0300

    Remoções arquivos desnecessários

--- backend/database/migrations/001-adicionar-mes.sql
+++
@@ -1,4 +0,0 @@
--- Execute uma única vez em bancos que já possuem a tabela AGENDAMENTOS.
--- Os registros existentes pertenciam ao calendário fixo de setembro.
-ALTER TABLE AGENDAMENTOS
-ADD COLUMN mes TINYINT UNSIGNED NOT NULL DEFAULT 9 AFTER dia;
